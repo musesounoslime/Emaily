aa423f244380aa29883f22a2aab627dcbc56f6b5
Zhuang <619787108@qq.com>
2023-07-18 11:38:48 -0400

    server implement

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// label and text input
+// if touched is true and error exist, show error
+export default ({ input, label, meta:{ error, touched } }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} style={{ marginBottom: '5px' }} />
+      <div className="red-text" style={{ marginBottom: '20px' }}>
+        {touched && error}
+      </div>
+    </div>
+  );
+};
